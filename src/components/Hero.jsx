8e8e3a9920f3bb46d35c98b47812538eb3605783
Steven Perez <stevenalp18@gmail.com>
2023-10-01 19:22:55 -0400

    attempt to load images after deploymnet again

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,64 @@
-import { IMAGE_CAROUSEL } from "../data/imageData.js";
 import { useEffect, useState } from "react";
+
+export const IMAGE_CAROUSEL = [
+  {
+    id: 0,
+    image: {
+      url: "/assets/exterior-home-1.jpeg",
+
+      alt: "Large Exterior of home being painted",
+    },
+  },
+  {
+    id: 1,
+    image: {
+      url: "/assets/kitchen-remodel-1.jpeg",
+      alt: "Kitchen Remodeling Finished Work",
+    },
+  },
+  {
+    id: 2,
+    image: {
+      url: "/assets/kitchen-remodel-2.jpeg",
+      alt: "Changed Countertop and Backsplash in Kitchen",
+    },
+  },
+  {
+    id: 3,
+    image: {
+      url: "/assets/kitchen-remodel-3.jpeg",
+      alt: "Cabinets Repainted in kitchen",
+    },
+  },
+  {
+    id: 4,
+    image: {
+      url: "/assets/basement-floor-1.jpeg",
+      alt: "Changed the basement wooden floors",
+    },
+  },
+  {
+    id: 5,
+    image: {
+      url: "/assets/bathroom-renovation-1.jpeg",
+      alt: "Renovated the bathroom completely",
+    },
+  },
+  {
+    id: 6,
+    image: {
+      url: "/assets/patio-paint.jpeg",
+      alt: "Patio Painted in gray",
+    },
+  },
+  {
+    id: 7,
+    image: {
+      url: "/assets/deck-work-1.jpeg",
+      alt: "Deck cleaned, prepped, power washed, and stained",
+    },
+  },
+];
 
 export default function Hero() {
   const [count, setCount] = useState(0);
@@ -31,7 +90,7 @@ export default function Hero() {
             <div className="my-auto rounded-xl px-6 pb-6">
               <div className="text-center">
                 <img
-                  src="src/assets/logo-black.svg"
+                  src="/assets/logo-black.svg"
                   alt="Black Logo"
                   width={200}
                   className="m-auto "
