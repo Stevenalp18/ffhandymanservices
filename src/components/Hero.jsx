b0f6cebaf04506b3fbe7a02ba516b75f8182275a
Steven Perez <stevenalp18@gmail.com>
2023-10-03 23:15:36 -0400

    added dropdown, still in making

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -84,7 +84,7 @@ export default function Hero() {
           <img
             src={currentImage.url}
             alt={IMAGE_CAROUSEL[count].image.alt}
-            className="w-full h-full object-cover blur-[2px]"
+            className="w-full h-full object-cover blur-[2px] opacity-60"
           />
           <div className="absolute left-0 right-0 top-0 bottom-40 m-auto w-4/5 md:w-3/5 lg:w-2/5 text-center flex justify-center align-middle">
             <div className="my-auto rounded-xl px-6 pb-6">
@@ -102,32 +102,6 @@ export default function Hero() {
                   DC | MD | VA
                 </p>
               </div>
-              {/* <div className="flex justify-center gap-4">
-                <button
-                  className="relative bg-slate-100/50 p-1 rounded-xl"
-                  onClick={() => {
-                    if (count - 1 === -1) {
-                      setCount(IMAGE_CAROUSEL.length - 1);
-                    } else if (count !== -1) {
-                      setCount(count - 1);
-                    }
-                  }}
-                >
-                  <img src="./left-arrow.png" width={24} alt="left-arrow" />
-                </button>
-                <button
-                  className="relative bg-slate-100/50 rounded-xl p-1"
-                  onClick={() => {
-                    if (count + 1 === IMAGE_CAROUSEL.length) {
-                      setCount(count - IMAGE_CAROUSEL.length + 1);
-                    } else if (count < IMAGE_CAROUSEL.length) {
-                      setCount(count + 1);
-                    }
-                  }}
-                >
-                  <img src="./right-arrow.png" width={24} alt="right-arrow" />
-                </button>
-              </div> */}
             </div>
           </div>
         </div>
